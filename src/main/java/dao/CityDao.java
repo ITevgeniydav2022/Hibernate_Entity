41165259d207cadebb89cb589aa25a487c2bd851
ITevgeniydav2022 <defox2022@gmail.com>
2023-02-25 14:29:56 +0300

    Сделано ДЗ Hibernate_Entity

---
+++ src/main/java/dao/CityDao.java
@@ -0,0 +1,15 @@
+package dao;
+import model.City;
+import java.util.List;
+import java.util.Optional;
+
+public interface CityDao {
+
+
+    City add(City city);
+
+    Optional<City> findById(long id);
+    List<City> findAll();
+    City update(City city);
+    Optional<City> delete(City city);
+}
